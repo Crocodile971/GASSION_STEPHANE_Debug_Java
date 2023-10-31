e6d1de9da27e10a5d32931089419d556711b4fa5
crocodile971 <gassion.stephane@gmail.com>
2023-10-31 09:06:23 +0100

    main implementation

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,19 +1,11 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class AnalyticsCounter {
-
-    private static int headacheCount = 0;
-    private static int rashCount = 0;
-    private static int pupilCount = 0;
     private ISymptomReader reader;
     private ISymptomWriter writer;
 
@@ -22,16 +14,20 @@ public class AnalyticsCounter {
         this.writer = writer;
     }
 
-    List<String> filepath = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt").GetSymptoms();
+    List<String> filePath = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt").getSymptoms();
 
     /**
      * @return return file with symptoms
      */
-    public List<String> getSymptoms() throws IOException {
+    public List<String> getSymptoms() {
+        try {
+            reader = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
 
-        reader = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
-
-        return reader.GetSymptoms();
+        } catch (Exception e) {
+            System.out.println("Unable to read file" + e.getMessage());
+            e.printStackTrace();
+        }
+        return reader.getSymptoms();
     }
 
     /**
@@ -40,19 +36,22 @@ public class AnalyticsCounter {
      */
     public Map<String, Integer> countSymptoms(List<String> symptoms) {
 
-        List<String> result = filepath;
-
+        List<String> result = filePath;
         Map<String, Integer> counterSymptoms = new HashMap<String, Integer>();
-
-        for (String symptom : result) {
-            // on vérifie si le symptom est dèjà présent, alors on incrémente le nombre d'élément
-            if (counterSymptoms.containsKey(symptom)) {
-                int nbConterSymptom = counterSymptoms.get(symptom);
-                counterSymptoms.put(symptom, nbConterSymptom + 1);
-            } else {// ajout du symptom en clé , valeur
-                counterSymptoms.put(symptom, 1);
+        try {
+            for (String symptom : result) {
+                // We check if the symptom is already present, then we increment the number of elements.
+                if (counterSymptoms.containsKey(symptom)) {
+                    int nbCounterSymptom = counterSymptoms.get(symptom);
+                    counterSymptoms.put(symptom, nbCounterSymptom + 1);
+                } else {
+                    // Adding the symptom in key, value.
+                    counterSymptoms.put(symptom, 1);
+                }
             }
-
+        } catch (Exception e) {
+            System.out.println("Impossible to count symptoms" + e.getMessage());
+            e.printStackTrace();
         }
         return counterSymptoms;
     }
@@ -63,15 +62,22 @@ public class AnalyticsCounter {
      * @return filter files alphabetically
      */
     public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
-        Map<String, Integer> sortSymptom = new TreeMap<>(countSymptoms(filepath));
+        Map<String, Integer> sortSymptom = new TreeMap<>(countSymptoms(filePath));
+        try {
+            for (Map.Entry<String, Integer> entry : sortSymptom.entrySet()) {
+                if (sortSymptom.containsKey(entry.getKey())) {
+                    // We check if the key is present and add it with the value.
+                    sortSymptom.put(entry.getKey(), entry.getValue());
+                } else {
+                    // Adding the symptom with nominal value.
+                    sortSymptom.put(entry.getKey(), 1);
 
-        for (Map.Entry<String, Integer> entry : sortSymptom.entrySet()) {
-            if (sortSymptom.containsKey(entry.getKey())) {
-                sortSymptom.put(entry.getKey(), entry.getValue() + 1);
-            } else {
-                sortSymptom.put(entry.getKey(), 1);
-
+                }
             }
+
+        } catch (Exception e) {
+            System.out.println("Sort alphabetically not possible" + e.getMessage());
+            e.printStackTrace();
         }
 
         return sortSymptom;
@@ -80,57 +86,16 @@ public class AnalyticsCounter {
 
     /**
      * @param symptoms list of symptoms in the filter file
-     * @throws IOException
      */
-    public void writeSymptoms(Map<String, Integer> symptoms) throws IOException {
-
-        writer = new WriteSymptomDataToFile("Project02Eclipse/result.out");
-        writer.writeSymptoms(symptoms);
-    }
-
-
-
-    public static void main(String[] args) throws Exception {
-
-
+    public void writeSymptoms(Map<String, Integer> symptoms) {
         try {
-            FileReader fileReader = new FileReader("Project02Eclipse/symptoms.txt");
+            writer = new WriteSymptomDataToFile("Project02Eclipse/result.out");
+            writer.writeSymptoms(symptoms);
 
-
-            BufferedReader reader = new BufferedReader(fileReader);
-            String line = reader.readLine();
-
-            int i = 0;
-            int headCount = 0;
-            while (line != null) {
-                i++;
-                System.out.println("symptom from file: " + line);
-                if (line.equals("headache")) {
-                    headCount++;
-                    System.out.println("number of headaches: " + headCount);
-                } else if (line.equals("rash")) {
-                    rashCount++;
-                } else if (line.contains("pupils")) {
-                    pupilCount++;
-                }
-
-                line = reader.readLine();
-            }
-            reader.close();
-        } catch (IOException e) {
+        } catch (Exception e) {
+            System.out.println("Unable to write file" + e.getMessage());
             e.printStackTrace();
         }
-
-        
-        
-        
-
-        FileWriter writer = new FileWriter("result.out");
-        writer.write("headache: " + headacheCount + "\n");
-        writer.write("rash: " + rashCount + "\n");
-        writer.write("dialated pupils: " + pupilCount + "\n");
-        writer.close();
     }
-
 
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -4,7 +4,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 public class WriteSymptomDataToFile implements ISymptomWriter {
@@ -32,22 +31,17 @@ public class WriteSymptomDataToFile implements ISymptomWriter {
             }
         else {
             try {
-                 FileWriter fileWriter = new FileWriter(file);
+                FileWriter fileWriter = new FileWriter(file);
                 BufferedWriter writer = new BufferedWriter(fileWriter);
 
                 for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
-                    String key = entry.getKey();
-                    Integer value = entry.getValue();
 
-                    writer.write("symptom from file: " + key + " : " + value);
+                    writer.write("symptom from file: " + entry.getKey() + " : " + entry.getValue());
                     writer.newLine();
-
-
-
                 }
                 writer.close();
             } catch (IOException e) {
-                System.out.println("Défaillance lors du chargement, erreur: " + e.getMessage());
+                System.out.println("Failure while loading error " + e.getMessage());
                 e.printStackTrace();
             }
         }
